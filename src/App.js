66edb402101f737dbcc1affc41e5d5f9101cfead
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-03-31 00:21:20 +0530

    config ui code implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import boyimage from"./images/Boy.jpg";
+import {PROFILE_IMG, NAME, ROLE, Description ,Button1, Button2} from "./config";
 
 function App() {
   return (
@@ -7,16 +7,16 @@ function App() {
       <h1 className='text-center font-bold text-2xl mt-1'>Profile Card</h1>
       <div className='h-auto w-2/4 mx-auto my-10 border-solid border-black border-black-300 border-4 p-10 rounded-xl shadow-lg shadow-gray-300'>
           <div className='flex justify-center'>
-             <img src={boyimage} height={180} width={180} alt='boy profile' className='rounded-full border-solid border-black border-4'/>
+             <img src={PROFILE_IMG} height={120} width={150} alt='profile' className='rounded-full border-solid border-black border-4'/>
           </div> 
           <div>
-            <h2 className='text-center mt-2 font-bold text-2xl'>Vicky Kaushal</h2>
-            <p className='text-center mt-1 font-medium text-xl'>Actor</p>
-            <p className='text-center m-1'>Kaushal was born on 16 May 1988 in a suburban chawl in Mumbai to Sham Kaushal, an action director in Indian films, and Veena Kaushal, a homemaker. His younger brother, Sunny, is also an actor. His family is Punjabi Hindu with their ancestral roots in Hoshiarpur, Punjab.</p>
+            <h2 className='text-center mt-2 font-bold text-2xl'>{NAME}</h2>
+            <p className='text-center mt-1 font-medium text-xl'>{ROLE}</p>
+            <p className='text-center m-1'>{Description}</p>
           </div>
           <div className='flex justify-center'>
-            <button className='bg-blue-400 p-2 m-3 rounded-lg text-white hover:translate-x-1'>Connect</button>
-            <button className='bg-blue-400 p-2 m-3 rounded-lg text-white hover:translate-x-1'>Message</button>
+            <button className='bg-blue-400 p-2 m-3 rounded-lg text-white hover:translate-x-1'>{Button1}</button>
+            <button className='bg-blue-400 p-2 m-3 rounded-lg text-white hover:translate-x-1'>{Button2}</button>
           </div>
       </div>
     </div>
---
+++ src/config.js
@@ -0,0 +1,10 @@
+import girl from "./images/Girl.jpg";
+
+const PROFILE_IMG = girl;
+const NAME = "Shraddha Kapoor";
+const ROLE = "Actor";
+const Description = "Shraddha Kapoor (born 3 March 1987) is an Indian actress who primarily works in Hindi films. One of India's highest-paid actresses, Kapoor has been featured in Forbes India's Celebrity 100 list since 2014 and was featured by Forbes Asia in their 30 Under 30 list of 2016."
+const Button1 = "Contact";
+const Button2 = "Message";
+
+export {PROFILE_IMG, NAME, ROLE, Description ,Button1, Button2};
